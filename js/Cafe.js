82e209fcfaa54b6abda4d50ff736de644bfa6ad6
Isidro <Isidre10_92@hotmail.es>
2017-02-22 20:40:58 +0100

    add header (mobile version)

---
+++ js/Cafe.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	var menu_activado = false;
+
+	$("header span#contenedor_menu_movil").click(function(){
+		/*
+		if (!menu_activado) {
+			$("nav ul#botonera_menu").show();
+			menu_activado = true;
+		}
+		else {
+			$("nav ul#botonera_menu").hide();
+			menu_activado = false;
+		}*/
+		
+	});
+
+});
